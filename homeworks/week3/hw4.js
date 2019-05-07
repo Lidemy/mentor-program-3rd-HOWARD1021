3ab9fae0a8a4c091a6d266f84b1c6db9c2af7b24
Howard <ms04626730@gmail.com>
2019-05-07 21:46:17 +0800

    'hw4'

--- homeworks/week3/hw4.js
+++ homeworks/week3/hw4.js
@@ -1,5 +1,11 @@
 function isPalindromes(str) {
-  console.log(str);
+  for (let i = 0; i < str.length - 1; i += 1) {
+    if (str[i] !== str[str.length - i - 1]) return false;
+  }
+  return true;
 }
+
+console.log(isPalindromes('abcdcba'));
+
 
 module.exports = isPalindromes;
