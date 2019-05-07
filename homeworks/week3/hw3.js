5b02db610d4887cc9de1ce11087a360663400849
Howard <ms04626730@gmail.com>
2019-05-07 18:37:54 +0800

    'hw3'

--- homeworks/week3/hw3.js
+++ homeworks/week3/hw3.js
@@ -3,3 +3,16 @@ function isPrime(n) {
 }
 
 module.exports = isPrime;
+function isprime(n) {
+  if (n === 1) return false;
+  for (let i = 2; i <= Math.sqrt(n); i += 1) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+console.log(isprime(7));
+
+module.exports = isprime;
