711a8a789e5a622858ce846ae1aa0d2671537899
Howard <ms04626730@gmail.com>
2019-05-14 23:37:04 +0800

    'hw3'

--- homeworks/week4/hw3.js
+++ homeworks/week4/hw3.js
@@ -1,62 +1,63 @@
 const request = require('request');
 const process = require('process');
 
-switch (process.argv[2]) {
-  case 'list':
-    request('https://lidemy-book-store.herokuapp.com/books?_limit=20',
-      (error, response, body) => {
-        const obj = JSON.parse(body);
-        obj.forEach((item) => {
-          console.log(`${item.id} ${item.name}`);
-        });
+if (process.argv[2] === 'list') {
+  request('https://lidemy-book-store.herokuapp.com/books?_limit=20',
+    (error, response, body) => {
+      const obj = JSON.parse(body);
+      obj.forEach((item) => {
+        console.log(`${item.id} ${item.name}`);
       });
-    break;
+    });
+}
+if (process.argv[2] === 'read') {
+  request.get(`https://lidemy-book-store.herokuapp.com/books/${process.argv[3]}`,
+    (error, response, body) => {
+      const obj = JSON.parse(body);
+      console.log(obj.name);
+    });
+}
+if (process.argv[2] === 'delete') {
+  request.delete(`https://lidemy-book-store.herokuapp.com/books/${process.argv[3]}`,
+    (error, response) => {
+      if (response.statusCode === 200) {
+        console.log('刪除');
+      } else if (response.statusCode === 404) {
+        console.log('無此項目');
+      } else {
+        console.log(response.statusCode);
+      }
+    });
+}
+if (process.argv[2] === 'create') {
+  const URL = 'https://lidemy-book-store.herokuapp.com/books';
+  request.post({
+    url: URL,
+    name: process.argv[3],
+  },
+  (error, response) => {
+    if (response.statusCode === 201) {
+      console.log('成功新增');
+    }
+  });
+}
+if (process.argv[2] === 'update') {
+  request.patch({
+    url: `https://lidemy-book-store.herokuapp.com/books/${process.argv[3]}`,
+    form: {
+      name: process.argv[4],
+    },
+  }, (error, response) => {
+    if (response.statusCode === 200) {
+      console.log('成功修改');
+    } else if (response.statusCode === 404) {
+      console.log('查無此項目');
+    } else {
+      console.log(response.statusCode);
+    }
+  });
 
-  case 'read':
-    request(`https://lidemy-book-store.herokuapp.com/books/${process.argv[3]}`,
-      (error, response, body) => {
-        const obj = JSON.parse(body);
-        console.log(obj.name);
-      });
-    break;
-
-  case 'delete':
-    request.delete(`https://lidemy-book-store.herokuapp.com/books/${process.argv[3]}`,
-      () => {
-        console.log(`刪除 id 為 ${process.argv[3]} 的書籍`);
-      });
-    break;
-
-  case 'create':
-    request.post(
-      {
-        url: 'https://lidemy-book-store.herokuapp.com/books/',
-        form: {
-          id: '',
-          name: process.argv[3],
-        },
-      },
-      () => {
-        console.log(`新增一本名為 ${process.argv[3]} 的書`);
-      },
-    );
-    break;
-
-  case 'update':
-    request.patch(
-      {
-        url: `https://lidemy-book-store.herokuapp.com/books/${process.argv[3]}`,
-        form: {
-          name: process.argv[4],
-        },
-      },
-      () => {
-        console.log(`更新 id 為 ${process.argv[3]} 的書名為 ${process.argv[4]}`);
-      },
-    );
-    break;
-
-  default:
-    console.log('請輸入正確的指令噢');
-    break;
+  if (process.argv[2] === undefined) {
+    console.log('Invalid input, please try again');
+  }
 }
