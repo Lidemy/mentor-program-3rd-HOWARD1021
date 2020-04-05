f7ff992a999c57570beffc9056ed53913de2709c
HOWARD1021 <ms04626730@gmail.com>
2020-04-05 20:53:54 +0800

    'blog_'

--- homeworks/week21/fe/hw3/App.js
+++ homeworks/week21/fe/hw3/App.js
@@ -5,20 +5,22 @@ import About from './about';
 import Home from './home'; 
 import Post from './post'; 
 
-
-
+// 顯示出# 後面的字元
 const _changeHashTag =  (str) => {
 return str.slice(1)
 }
 
+// 如果 現在的字元 與 path 相同 則呼叫 那個 Comp
 class Route extends Component{
-render(){
+  render(){
   const {current, path, Comp} = this.props
   return current !== path ? null: <Comp/>
-}
+  }
 }
 
 
+// 設定用來判斷 路由的 tab
+// 使用 window.location.hash 可以獲得現在的 #hashtag 字元  
 class App extends Component {
   constructor(){
     super()
@@ -27,28 +29,26 @@ class App extends Component {
     }
   }
 
-
+// 當 hashchange 當 hash 改變時 呼叫 handlehashchange
 componentDidMount(){
   window.addEventListener("hashchange",this.handlehashchange)
 }
 componentWillUnomount(){
   window.remoeEventListener(this.handlehashchange)
 }
-  
+// 我們會改變 現在的 #hashtag
 handlehashchange = ()=>{
 this.setState({
   tab:_changeHashTag(window.location.hash)
 })
 } 
 
-
-  
   render(){
     const {tab} = this.state
     return (
     <div className='App'>
     <Nav  activeTab={tab}/>
-    <div className='page'>    
+    <div className='page'> 
 <Route current={tab} path={'home'} Comp={Home}/>
 <Route current={tab} path={'about'} Comp={About}/>
 <Route current={tab} path={'post'} Comp={Post}/>
